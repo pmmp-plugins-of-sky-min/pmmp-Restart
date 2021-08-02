8bdb0b85673ade73b86ea20ba24b79a5823bc425
sky-min <81374952+sky-min@users.noreply.github.com>
2021-08-02 14:11:04 +0900

    Create [SKY]Restart.php

---
+++ [SKY]Restart.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @name DragonDeathEffect
+ * @main skymin\server\Restart
+ * @author skymin
+ * @version SKY
+ * @api 3.0.0
+ */
+
+declare(strict_types = 1);
+
+namespace skymin\server;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+
+class Restart extends PluginBase{
+  
+  private const DELAY = 30;
+  
+  public function onEnable():void{
+    
+  }
+  
+  public function restart():void{
+    
+  }
+  
+}
